794ee255db127ee5c56f54333f9d34b667bcf59d
hjw961004 <hjw961004@gmail.com>
2021-11-04 17:09:57 +0900

    update graph
    그래프 업데이트

--- src/WorldData/FrameGraph.java
+++ src/WorldData/FrameGraph.java
@@ -15,7 +15,7 @@ public class FrameGraph extends JFrame implements ActionListener{
 	
 	public FrameGraph() {
 		super("그래프");
-		setSize(800,600);
+		setSize(778,630);
 		setResizable(false);
 		setLocationRelativeTo(null);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -31,9 +31,9 @@ public class FrameGraph extends JFrame implements ActionListener{
 		btnBack.addActionListener(this);
 	}
 	
-	public FrameGraph(Info a) {
+	public FrameGraph(String n1, double a1) {
 		super("그래프");
-		setSize(800,600);
+		setSize(778,630);
 		setResizable(false);
 		setLocationRelativeTo(null);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -41,13 +41,51 @@ public class FrameGraph extends JFrame implements ActionListener{
 		btnBack = new JButton("뒤로가기");
 		add(btnBack, BorderLayout.NORTH);
 
-		drawingPanel = new DrawingPanel(a);
+		drawingPanel = new DrawingPanel(n1, a1);
 		add(drawingPanel, BorderLayout.CENTER);
 
 		setVisible(true);
 		
 		btnBack.addActionListener(this);
 	}
+	
+	public FrameGraph(String n1, double a1, String n2, double a2) {
+		super("그래프");
+		setSize(778,630);
+		setResizable(false);
+		setLocationRelativeTo(null);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		btnBack = new JButton("뒤로가기");
+		add(btnBack, BorderLayout.NORTH);
+
+		drawingPanel = new DrawingPanel(n1, a1, n2, a2);
+		add(drawingPanel, BorderLayout.CENTER);
+
+		setVisible(true);
+		
+		btnBack.addActionListener(this);
+	}
+	
+
+	public FrameGraph(String n1, double a1, String n2, double a2, String n3, double a3) {
+		super("그래프");
+		setSize(778,630);
+		setResizable(false);
+		setLocationRelativeTo(null);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		btnBack = new JButton("뒤로가기");
+		add(btnBack, BorderLayout.NORTH);
+
+		drawingPanel = new DrawingPanel(n1, a1, n2, a2, n3, a3);
+		add(drawingPanel, BorderLayout.CENTER);
+
+		setVisible(true);
+		
+		btnBack.addActionListener(this);
+	}
+
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
@@ -67,62 +105,113 @@ public class FrameGraph extends JFrame implements ActionListener{
 
 class DrawingPanel extends JPanel{
 	int korea = 100, america = 50, japan = 20;
+	int Sx = 30, Rx = 700, Sy = 30, Ry = 500;
 	
-	String infoName1, infoName2, infoName3, infoName4, infoName5;
-	int infoArea1, infoArea2, infoArea3, infoArea4, infoArea5;
+	String infoName1, infoName2, infoName3;
+	double infoArea1, infoArea2, infoArea3;
+	int pArea1, pArea2, pArea3;
+	
 	int count;
-	int maxArea;
+	double maxArea;
 	
 	public DrawingPanel() {
+		count = 0;
+	}
+	
+	public DrawingPanel(String n1, double a1) {
 		count = 1;
 	}
 	
-	public DrawingPanel(Info a) {
-		infoName1 = a.name;
-		infoArea1 = Integer.parseInt(a.area);
-		count = 1;
-		maxArea = infoArea1;
+	public DrawingPanel(String n1, double a1, String n2, double a2) {
+		infoName1 = n1;
+		infoArea1 = a1;
+		
+		infoName2 = n2;
+		infoArea2 = a2;
+		
+		count = 2;
+		
+		if(a1 >= a2)
+			maxArea = a1;
+		else
+			maxArea = a2;
+		
+		pArea1 = (int) Math.round(infoArea1 / maxArea * 100);
+		pArea2 = (int) Math.round(infoArea2 / maxArea * 100);
+	}
+	
+	public DrawingPanel(String n1, double a1, String n2, double a2, String n3, double a3) {
+		infoName1 = n1;
+		infoArea1 = a1;
+		
+		infoName2 = n2;
+		infoArea2 = a2;
+		
+		infoName3 = n3;
+		infoArea3 = a3;
+		
+		count = 3;
+		
+		if(a1 >= a2)
+			maxArea = a1;
+		else
+			maxArea = a2;
+		
+		if(a3 > maxArea)
+			maxArea = a3;
+			
+		
+		pArea1 = (int) Math.round(infoArea1 / maxArea * 100);
+		pArea2 = (int) Math.round(infoArea2 / maxArea * 100);
+		pArea3 = (int) Math.round(infoArea3 / maxArea * 100);
 	}
 	
 	public void paint(Graphics g){
+		g.clearRect(0,0,getWidth(),getHeight());
+		g.drawRect(Sx,Sy,Rx,Ry);
+		g.drawLine(Sx+50, Sy+Ry-50, Sx+Rx-50, Sy+Ry-50);
+		g.drawLine(Sx+50, Sy+50, Sx+50, Sy+Ry-50);
+		
 		switch(count) {
-		case 0 :
-			g.drawString("비교 국가가 없습니다.", 100, 100);
+		case 0  :
+			g.drawString("비교 대상을 최소 두 개 이상 선택해주세요.", Sx+250, Sy+200);
+				
 			break;
 		case 1 :
-			g.clearRect(0,0,getWidth(),getHeight());
-			g.drawLine(1,509,789,509);
+			g.drawString("비교 대상을 최소 두 개 이상 선택해주세요.", Sx+250, Sy+200);
 			
-			/*
-			g.clearRect(0,0,getWidth(),getHeight());
-			g.drawLine(50,250,350,250);
-			
-			for(int cnt = 1 ; cnt<11 ; cnt++){
-				g.drawString(cnt *10 +"",25,255-20*cnt);
-				g.drawLine(50, 250-20*cnt, 350,250-20*cnt);
-			}
-			
-			g.drawLine(50,20,50,250);
-			g.drawString("한국",100,270);
-			g.drawString("미국",200,270);
-			g.drawString("일본",300,270);
-			g.setColor(Color.RED);
-			
-			if(korea>0)
-				g.fillRect(110,250-korea*2,10,korea*2);
-			if(america>0)
-				g.fillRect(210,250-america*2,10,america*2);
-			if(japan>0)
-				g.fillRect(310,250-japan*2,10,japan*2);
-				*/
 			break;
 		case 2 :
+			for(int cnt = 1 ; cnt < 11 ; cnt++){
+				g.drawString(cnt *10 +"%", Sx+15, Sy+Ry-45-40*cnt);
+				g.drawLine(Sx+50, Sy+Ry-50-40*cnt, Sx+Rx-50, Sy+Ry-50-40*cnt);
+			}
+			
+			g.drawString(infoName1 + "(" + infoArea1 + ")" , Sx+50+120, Sy+Ry-30);
+			g.drawString(infoName2 + "(" + infoArea2 + ")", Sx+50+270, Sy+Ry-30);
+			
+			g.setColor(Color.RED);
+			
+			g.fillRect(Sx+50+120,Sy+Ry-50-pArea1*4,60,pArea1*4);
+			g.fillRect(Sx+50+270,Sy+Ry-50-pArea2*4,60,pArea2*4);
+			
 			break;
 		case 3 :
-			break;
-		case 4 :
-			break;
-		case 5 :
+			for(int cnt = 1 ; cnt < 11 ; cnt++){
+				g.drawString(cnt *10 +"%", Sx+15, Sy+Ry-45-40*cnt);
+				g.drawLine(Sx+50, Sy+Ry-50-40*cnt, Sx+Rx-50, Sy+Ry-50-40*cnt);
+			}
+			
+			g.drawString(infoName1 + "(" + infoArea1 + ")" , Sx+50+120, Sy+Ry-30);
+			g.drawString(infoName2 + "(" + infoArea2 + ")", Sx+50+270, Sy+Ry-30);
+			g.drawString(infoName3 + "(" + infoArea3 + ")", Sx+50+420, Sy+Ry-30);
+			
+			g.setColor(Color.RED);
+			
+			g.fillRect(Sx+50+120,Sy+Ry-50-pArea1*4,60,pArea1*4);
+			g.fillRect(Sx+50+270,Sy+Ry-50-pArea2*4,60,pArea2*4);
+			g.fillRect(Sx+50+420,Sy+Ry-50-pArea3*4,60,pArea3*4); 
+			
 			break;
 		default :
 			break;
